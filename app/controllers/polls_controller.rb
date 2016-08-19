087ec284448acc8cbd1d03806380d32af9e080da
cartagena <jcartagena93@gmail.com>
2016-08-19 19:11:15 -0400

    builds mock JSON

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,6 +1,30 @@
 class PollsController < ApplicationController
+
   def index
     @polls = Poll.all
-    render json: @polls
+    render json: results
   end
+
+  def results
+    @results = {}
+    @questions = []
+
+    @polls.map do |poll|
+      @results[poll.name] = add_questions(poll)
+    end
+    @results
+  end
+
+
+  def add_questions(poll)
+    poll.questions.map do |question|
+      @questions << Hash[question.prompt, add_options(question)]
+    end
+    @questions
+  end
+
+  def add_options(question)
+    question.options.map { |option| Hash[option.name, option.score] }
+  end
+
 end
